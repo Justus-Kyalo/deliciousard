4760b90686aa5a6c08f48445c988cd82191e28eb
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-25 01:32:16 +0300

    fetched data and rendered

--- src/Components/FetchData.js
+++ src/Components/FetchData.js
@@ -1,21 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import "./css/Fetchdata.css"
 
 function FetchData() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://grocery-uo79.onrender.com/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
     <div>
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+      {items.map((item)=>{
+        return(
+        <div key={item.id}>
+        <div className="card" style={{width: '18rem'}}>
+          <img src= {item.image} className="card-img-top" alt={item.productName} />
+          <div className="card-body">
+            <h5 className="card-title">{item.productName}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="##" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
         </div>
-      </div>
+        </div>)
+      })}
     </div>
   );
 }
