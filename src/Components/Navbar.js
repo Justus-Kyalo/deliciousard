e621ebefeeca0e6e9635f5a4f4963231cf418eac
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-25 00:36:57 +0300

    debugged code changing classes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Navbar() {
-    return (        
-        <nav class="navbar bg-light">
-            <div class="container-fluid">
-            <a class="navbar-brand">Deliciousard</a>
-            <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search item.." aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-      <a href="#"> <i class="fas fa-shopping-cart"></i></a> 
+    return (    <>    
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+            <a href='##' className="navbar-brand">Deliciousard</a>
+            <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search item.." aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+      <a href="##"> <i className="fas fa-shopping-cart"></i></a> 
     </form>
   </div>
 </nav>
+</>
         
     );
 }
